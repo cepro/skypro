fc9bb55d20396f2bf723db4dd30964daed86a34f
Marcus Wood <marcus.wood@cepro.energy>
2024-06-20 14:37:46 +0100

    Allow approach to be turned off in config

--- src/skypro/commands/simulator/algorithms/approach.py
+++ src/skypro/commands/simulator/algorithms/approach.py
@@ -30,7 +30,7 @@ def get_peak_approach_energies(
     """
     # TODO: this approach won't work if the approach curve crosses over a midnight boundary
 
-    if not peak_config.period:
+    if not peak_config.period or peak_config.approach.to_soe == 0:
         return 0.0, 0.0
 
     t = t.astimezone(pytz.timezone(TIMEZONE_STR))
